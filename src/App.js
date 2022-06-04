cc7b5952c6d9693d833ffe68a9b18049e4053580
Sadman Chowdhury <sadman923@gmail.com>
2022-06-04 06:20:23 +0600

    Component tree  organization

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import MainComponent from "./components/MainComponent";
 
 function App() {
   return (
     <div className="App">
-     
+      <MainComponent/>
     </div>
   );
 }
---
+++ src/components/MainComponent.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Body from './body/Body';
+import Header from './header/Header';
+import Footer from './footer/Footer';
+
+const MainComponent = () => {
+  return <div>
+      <Header/>
+      <Body/>
+      <Footer/>
+  </div>;
+}
+
+export default MainComponent;
